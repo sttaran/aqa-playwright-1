fed0434c51fe30c40367e150fce0da907f136355
staran <staran@griddynamics.com>
2023-11-06 20:31:02 +0200

    staran // add configs

---
+++ config/config.js
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const config = {
+    baseURL: process.env.BASE_URL ?? "http://localhost",
+    httpCredentials: {
+        username: process.env.HTTP_CREDENTIALS_USERNAME,
+        password: process.env.HTTP_CREDENTIALS_PASSWORD
+    }
+}
